4618aa8e1229cd7a0a5c5c8754c9eaa8e4de5c05
David Martinez <dmartinezperez1@icloud.com>
2023-07-06 15:47:30 -0400

    Initial Commit

---
+++ DemoApp/DemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoAppApp.swift
+//  DemoApp
+//
+//  Created by David Martinez on 7/6/23.
+//
+
+import SwiftUI
+
+@main
+struct DemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
